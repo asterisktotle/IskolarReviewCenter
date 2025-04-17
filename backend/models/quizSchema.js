b98caa9e19f9e6d89565064060b43b60cee1271d
asterisktotle <barandajander@gmail.com>
2025-04-17 08:53:44 +0800

    quiz_schema

---
+++ backend/models/quizSchema.js
@@ -0,0 +1,97 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const QuestionSchema = new Schema({
+	type: {
+		type: String,
+		enum: ['multiple-choice', 'true-false', 'fill-in-the-blank'],
+		required: true,
+	},
+	options: [
+		{
+			text: String,
+			isCorrect: Boolean,
+		},
+	],
+
+	//for fill-in-the-blank
+	correctAnswer: {
+		type: String,
+	},
+	points: {
+		type: Number,
+		default: 1,
+	},
+});
+
+const QuizSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	subject: {
+		type: String,
+		required: true,
+	},
+	questions: [QuestionSchema],
+	totalPoints: {
+		type: Number,
+		default: 0,
+	},
+	passingScore: {
+		type: Number,
+		default: 70,
+	},
+	timeLimit: {
+		type: Number,
+		default: 240, // in minutes
+	},
+	createdDate: { timestamps: true },
+});
+
+const QuizAttemptSchema = new Schema({
+	quiz: {
+		type: Schema.Types.ObjectId,
+		ref: 'Quiz',
+		required: true,
+	},
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'user',
+		required: true,
+	},
+	startedAt: {
+		type: Date,
+		default: Date.now,
+	},
+	completedAt: {
+		type: Date,
+	},
+	answers: [
+		{
+			questionIndex: {
+				type: Number,
+				required: true,
+			},
+			questionText: String, // optional: store the actual question for record-keeping
+			selectedOptions: [String], // for multiple-choice or true/false
+			textAnswer: String, // for fill-in-the-blank
+			isCorrect: Boolean,
+			pointsEarned: Number,
+		},
+	],
+	score: {
+		type: Number,
+	},
+	percentageScore: {
+		type: Number,
+	},
+	passed: {
+		type: Boolean,
+	},
+});
+
+const Quiz = mongoose.model('Quiz', QuizSchema);
+const QuizAttempt = mongoose.model('QuizAttempt', QuizAttemptSchema);
+
+export { Quiz, QuizAttempt };
