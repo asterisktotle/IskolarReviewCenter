7ae2fe357554307e3abb396645974d439eb23e16
asterisktotle <barandajander@gmail.com>
2025-04-20 08:57:42 +0800

    update_delete_quizbackend

--- backend/routes/quiz-route.js
+++ backend/routes/quiz-route.js
@@ -1,4 +1,21 @@
 import express from 'express';
-import { userAuth } from '../middleware/userAuth';
+import { userAuth } from '../middleware/userAuth.js';
+import {
+	createQuiz,
+	getAllQuizzes,
+	submitAndEvaluateQuiz,
+	getUserQuizHistory,
+	deleteQuiz,
+	updateQuiz,
+} from '../controllers/featuresController.js';
 
 const quizRouter = express.Router();
+
+quizRouter.post('/create-quiz', userAuth, createQuiz);
+quizRouter.post('/submit-quiz', userAuth, submitAndEvaluateQuiz);
+quizRouter.delete('/delete-quiz/:id', userAuth, deleteQuiz);
+quizRouter.put('/update-quiz/:id', userAuth, updateQuiz);
+quizRouter.get('/get-all-quizzes', userAuth, getAllQuizzes);
+quizRouter.get('/get-user-quiz-history', userAuth, getUserQuizHistory);
+
+export default quizRouter;
